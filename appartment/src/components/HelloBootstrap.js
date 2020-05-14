bb9ba07145b4ef641665f448214e394ce0286644
Carolinams7 <57420037+Carolinams7@users.noreply.github.com>
2020-05-14 00:59:41 -0500

    added component-bootstrap

---
+++ appartment/src/components/HelloBootstrap.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HelloBootstrap = () => (
+    <div>
+        <nav className="navbar navbar-default">
+            <div className="container-fluid">
+                <form className="navbar-form navbar-center">
+                </form>
+            </div>
+        </nav>
+    </div>
+);
+
+export default HelloBootstrap;
