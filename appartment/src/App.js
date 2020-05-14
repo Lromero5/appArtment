4e7934dd8ed7e80a9a8ebfcf01c70f1dd1abe74b
Monica Rodriguez <monik_0491@hotmail.com>
2020-05-14 12:52:16 -0600

    Created App.js structure, stylingm, householdData.js file

--- appartment/src/App.js
+++ appartment/src/App.js
@@ -1,33 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-
-// import HelloBootstrap from "./components/HelloBootstrap";
-
-// const App = () => <HelloBootstrap />;
-
-// export default App;
-
+import householdData from './utils/householdData'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <div className="grid-container">
+        <header className="header">
+            <div className="brand">
+                <a href="index.html">
+                    <img src="/images/house.png" alt="logo-appartment" width="80" height="80"></img>
+                </a>
+            </div>
+            <div className="header-links">
+                <a href="home.html">Home</a>
+                <br/>
+                <a href="logout.html">Log Out</a>
+            </div>
+        </header>
+        
+        <main className="main">
+                <div classNameName="profile-info">
+                    <h2>Hi! = User</h2>
+                        <p>You currently belong to:</p>
+                            <ul classNameName="households">
+                              {
+                                householdData.households.map(household => 
+                                <li>
+                                    <div classNameName="houselhold">
+                                        <a href="household.html">{household.name}</a>
+                                    </div>
+                                </li>)
+                              }
+                            </ul>
+                </div>
+             
+                <div classNameName="join">
+                    <h3>Feeling FOMO?</h3>
+                    <form>
+                        <label>Join your roommates:</label>
+                        <input type="text" id="hname" name="hname" placeholder="Household name"/>
+                        <br/>
+                        <input type="submit" value="Join" classNameName="btn btn-warning"/>
+                    </form>
+                    <br/>
+                    <hr/>
+                </div>
+
+                <div classNameName="create">
+                    <form>
+                        <label>Create your own: </label>
+                        <input type="text" id="hname" name="hname" placeholder="Marcella St"/>
+                        <br/>
+                        <label>Add members:</label>
+                        <input type="text" id="hmember" name="hmember" placeholder="Emails here"/>
+                        <br/>
+                        <input type="submit" value="Create" classNameName="btn btn-warning"/>
+                        <br/>
+                    </form>
+                </div>
+      </main>
+                  
+        <footer className="footer">
+            <img src="./images/bless.png" alt="slogan" width="200" height="20"></img>
+        </footer>
+      </div>
   );
 }
 
---
+++ appartment/src/utils/householdData.js
@@ -0,0 +1,9 @@
+export default {
+    households: [
+    {
+        id:'',
+        name: '5th Street',
+        members: ''
+    }
+]
+}
