8e592f1d57dd1dfd536e93085ab3ef4a78a842bb
Carolinams7 <57420037+Carolinams7@users.noreply.github.com>
2020-05-23 15:04:58 -0500

    updated account file w chores import

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import Footer from '../components/Footer';
-import Members from '../components/InfoMembers'
-import Finances from '../components/Finances'
-import Calendar from '../components/Calendar'
+import React from "react";
+import Footer from "../components/Footer";
+import Members from "../components/InfoMembers";
+import Finances from "../components/Finances";
+import Calendar from "../components/Calendar";
+import Chores from "../components/Chores/Chores";
+
 // import householdData from '../utils/householdData';
 
-
-function Account(props){
-    console.log(props.match.params.id)
-    // const house = householdData.household.find(home => home.id === props.match.params.id)
-    return(
+function Account(props) {
+  console.log(props.match.params.id);
+  // const house = householdData.household.find(home => home.id === props.match.params.id)
+  return (
     <div>
-        <div className="head-title">
-            <h1>Hi user.name!</h1>
-        </div>
-        <Members/>
-        <Calendar/>
-        <Finances/>
-        <Footer/>
+      <div className="head-title">
+        <h1>Hi user.name!</h1>
+      </div>
+      <Members />
+      <Chores />
+      <Calendar />
+      <Finances />
+      <Footer />
     </div>
-    )
+  );
 }
 
 export default Account;
